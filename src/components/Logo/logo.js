a9253aa9f5c44e783916a2a0039a9a8aed139a2f
chris <christopherex4@gmail.com>
2021-05-01 14:28:58 +0100

    basic logo

---
+++ src/components/Logo/logo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {ReactComponent as Brand} from '../../assets/_cashenden.svg';
+
+function Logo() {
+    return <Brand />
+};
+
+
+export default Logo;
---
+++ src/pages/Home/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo from '../../components/Logo';
+import styles from './styles.module.scss';
+
+function Home() {
+    return <div className={styles.container}><Logo/></div>
+}
+
+export default Home;
