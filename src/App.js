4b63a2aff0479df41decc029b51dddb55237f8a3
chris <christopherex4@gmail.com>
2021-05-01 11:33:35 +0100

    tailwind added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="bg-gray-500">
           Hello
         </p>
         <a
